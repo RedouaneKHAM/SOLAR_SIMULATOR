34889c5b83ec0cfce2a15f3e250855a122bb8659
exec zsh <redouane.kham.study@gmail.com>
2024-02-01 20:42:50 +0100

    faq&home

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # get 'simulations/create'
   devise_for :users
   root to: "pages#home"
+  get "about", to: "pages#about"
+  get "faq", to: "pages#faq"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
