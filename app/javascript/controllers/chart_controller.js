89743e003fb5bcf7b19fd41a3c147fb4113f2e40
exec zsh <redouane.kham.study@gmail.com>
2024-02-05 16:01:29 +0100

    alert

--- app/javascript/controllers/chart_controller.js
+++ app/javascript/controllers/chart_controller.js
@@ -5,26 +5,34 @@ export default class extends Controller {
 
   static values = {
     chartdata1: String,
-    chartdata2: String
+    chartdata2: String,
+    chartdata3: String
   }
   connect() {
 
     new Chart(this.element, {
       type: 'line',
       data: {
-        labels: ["1","2","3","4","5","6","7","8","9","10"],
+        labels: ["2024","2025","2026","2027","2028","2029","2030","2031","2032","2033"],
         datasets: [{
-          label: 'My First Dataset',
+          label: 'Annual electricity expenses in DHS',
           data: JSON.parse(this.chartdata1Value),
           fill: false,
-          borderColor: 'rgb(75, 192, 192)',
+          borderColor: 'rgb(255, 0, 0)',
           tension: 0.1
         },
         {
-          label: 'My Second Dataset',
+          label: 'annual amount invested in DHS',
           data: JSON.parse(this.chartdata2Value),
           fill: false,
-          borderColor: 'rgb(0, 192, 0)',
+          borderColor: 'rgb(0, 128, 0)',
+          tension: 0.1
+        },
+        {
+          label: 'annual amount invested in DHS',
+          data: JSON.parse(this.chartdata3Value),
+          fill: false,
+          borderColor: 'rgb(0, 0, 255)',
           tension: 0.1
         }]
       }
--- app/models/simulation.rb
+++ app/models/simulation.rb
@@ -1,5 +1,5 @@
 class Simulation < ApplicationRecord
   belongs_to :user
   has_many :reports
-  validates :type_of_property, :region, :month_fst, :month_scd, :month_trd, :month_fth, presence: true, presence: { message: "must be given please" }
+  validates :type_of_property, :region, :month_fst, :month_scd, :month_trd, :month_fth, presence: { message: "must be given please" }
 end
