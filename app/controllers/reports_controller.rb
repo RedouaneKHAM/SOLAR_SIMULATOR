5809a07740494b1ed341aab35bc1e118cd3d8a11
exec zsh <redouane.kham.study@gmail.com>
2024-02-05 11:09:29 +0100

    homepage

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,24 @@
 class ReportsController < ApplicationController
-
   def show
     @report = Report.find(params[:id])
+    if @report.daily_consumption <= 420
+      @chartdata1 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+      @chartdata2 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+    elsif @report.daily_consumption <= 660
+      @chartdata1 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+      @chartdata2 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+    elsif @report.daily_consumption <= 1000
+      @chartdata1 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+      @chartdata2 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+    elsif @report.daily_consumption <= 1300
+      @chartdata1 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+      @chartdata2 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+    elsif @report.daily_consumption <= 3000
+      @chartdata1 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+      @chartdata2 = [6, 5, 5, 40, 56, 40, 81, 56, 95, 40]
+    else
+      flash[:notice] = "Contact us for more information"
+      redirect_to root_path
+    end
   end
 end
