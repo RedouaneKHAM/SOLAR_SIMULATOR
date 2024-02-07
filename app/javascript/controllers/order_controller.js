762e52ca38320de288556b58c93caa4c235ea645
exec zsh <redouane.kham.study@gmail.com>
2024-02-07 15:08:19 +0100

    mercredi

--- app/javascript/controllers/order_controller.js
+++ app/javascript/controllers/order_controller.js
@@ -1,14 +1,20 @@
 import { Controller } from "@hotwired/stimulus"
-
+import Swal from "sweetalert2"
 // Connects to data-controller="order"
 export default class extends Controller {
   connect() {
+    console.log("Hello, Stimulus!", this.element);
   }
-  showAlert() {
-    // Prevent default link behavior
-    // event.preventDefault();
-
-    // Show the alert message
-    alert("We will contact you soon");
+  showAlert(event) {
+    event.preventDefault();
+    Swal.fire({
+      position: "top-end",
+      icon: "success",
+      title: "Your work has been saved",
+      showConfirmButton: false,
+      timer: 1500
+    }).then(() => {
+        window.location.href = "/"
+    });
   }
 }
